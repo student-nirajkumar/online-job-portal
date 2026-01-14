3ffa94e917ef43778c7989cb536f05f709c81dcf
student-nirajkumar <neerajkumar89212@gmail.com>
2026-01-14 11:46:56 +0530

    Fix auth API integration and production environment config

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -90,7 +90,7 @@ const Signup = () => {
                             value={input.email}
                             name="email"
                             onChange={changeEventHandler}
-                            placeholder="patel@gmail.com"
+                            placeholder="niraj@gmail.com"
                         />
                     </div>
                     <div className='my-2'>
@@ -110,7 +110,7 @@ const Signup = () => {
                             value={input.password}
                             name="password"
                             onChange={changeEventHandler}
-                            placeholder="patel@gmail.com"
+                            placeholder="niraj@12345"
                         />
                     </div>
                     <div className='flex items-center justify-between'>
